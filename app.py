8c28e04654beea58c05ef76285bace33d27a5c05
Aashi Goel <aashi25goel@gmail.com>
2025-06-26 21:00:14 +0530

    rendered dynamic data using jinja2 technique

--- app.py
+++ app.py
@@ -2,9 +2,36 @@ from flask import Flask , render_template
 
 app = Flask(__name__) #creates the flask app but doesn't run it
 
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Software Engineer',
+        'location': 'Bangalore, India',
+        'salary': '₹15,00,000'
+    },
+    {
+        'id': 2,
+        'title': 'Data Analyst',
+        'location': 'Delhi, India',
+        'salary': '₹8,00,000'
+    },
+    {
+        'id': 3,
+        'title': 'Frontend Developer',
+        'location': 'Punjab, India',
+        # 'salary': '₹12,00,000'
+    },
+    {
+        'id': 4,
+        'title': 'Machine Learning Engineer',
+        'location': 'Mumbai, India',
+        'salary': '₹18,00,000'
+    }
+]
+
 @app.route("/") #registered a route for flask application
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html' , jobs = JOBS , company_name = 'Jovian')
 
 if __name__ == '__main__':
     app.run(debug = True)
