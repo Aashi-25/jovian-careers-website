53e29c99c7a308cb307fd2ff4e1fe099c613e9d9
Aashi Goel <aashi25goel@gmail.com>
2025-06-26 16:50:50 +0530

    added html and css

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__) #creates the flask app but doesn't run it
 
 @app.route("/") #registered a route for flask application
 def hello_world():
-    return "Hello World"
+    return "Hello, Jovian!"
 
 if __name__ == '__main__':
     app.run(debug = True)
