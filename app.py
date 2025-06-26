4094e086deb74387468be124f2db68208dc90b6f
Aashi Goel <aashi25goel@gmail.com>
2025-06-26 19:59:40 +0530

    added render template

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask , render_template
 
 app = Flask(__name__) #creates the flask app but doesn't run it
 
 @app.route("/") #registered a route for flask application
 def hello_world():
-    return "Hello, Jovian!"
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug = True)
